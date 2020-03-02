7268a4537dd6c945ff265277727883076c9ea2e2
Nick Pearson <nick.pearson@york.ac.uk>
2020-03-02 13:09:58 +0000

    Tidy up Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,14 @@
+
 module.exports = function(grunt) {
 
 	var snippetsURL = 'http://www.york.ac.uk/homepage-snippets/';
 	var currentDate = new Date();
 	var dateStamp =  currentDate.getFullYear() + '_' +
-                  ('0' + (currentDate.getMonth()+1)).slice(-2) + '_' +
-                  ('0' + currentDate.getDate()).slice(-2) + '_' +
-                  ('0' + currentDate.getHours()).slice(-2) +
-                  ('0' + currentDate.getMinutes()).slice(-2) +
-                  ('0' + currentDate.getSeconds()).slice(-2);
+	                 ('0' + (currentDate.getMonth()+1)).slice(-2) + '_' +
+	                 ('0' + currentDate.getDate()).slice(-2) + '_' +
+	                 ('0' + currentDate.getHours()).slice(-2) +
+	                 ('0' + currentDate.getMinutes()).slice(-2) +
+	                 ('0' + currentDate.getSeconds()).slice(-2);
 
 	// Project configuration.
 	grunt.initConfig({
@@ -27,62 +28,57 @@ module.exports = function(grunt) {
 		credentials: grunt.file.readJSON('.ftppass'),
 
 		sftp: {
+
 			test: {
-				files : {
-					'./': 'upload/index.shtml'
-				},
+				files : { './': 'upload/index.shtml' },
 				options: {
-          path: '/usr/yorkweb/web/static/data/homepage/',
-          host: 'sftp.york.ac.uk',
-          username: '<%= credentials.static.username %>',
-          password: '<%= credentials.static.password %>',
-          srcBasePath: 'upload/',
+					path: '/usr/yorkweb/web/static/data/homepage/',
+					host: 'sftp.york.ac.uk',
+					username: '<%= credentials.static.username %>',
+					password: '<%= credentials.static.password %>',
+					srcBasePath: 'upload/',
 					showProgress: true
 				}
-      },
-      preview: {
-        files : {
-          './': 'upload/index.shtml'
-        },
-        options: {
-          path: '/usr/yorkweb/web/preview/',
-          host: 'sftp.york.ac.uk',
-          username: '<%= credentials.preview.username %>',
-          password: '<%= credentials.preview.password %>',
-          srcBasePath: 'upload/',
-          showProgress: true
-        }
-      },
-      live: {
-        files : {
-          './': 'upload/index.shtml'
-        },
-        options: {
-          path: '.',
-          host: 'sftp.york.ac.uk',
-          username: '<%= credentials.live.username %>',
-          password: '<%= credentials.live.password %>',
-          srcBasePath: 'upload/',
-          showProgress: true
-        }
-      },
-      images: {
-        files : {
-          './': 'upload/images/**'
-        },
-        options: {
-          path: '/usr/yorkweb/web/static/data/homepage/images/',
-          host: 'sftp.york.ac.uk',
-          username: '<%= credentials.static.username %>',
-          password: '<%= credentials.static.password %>',
-          srcBasePath: 'upload/images/',
-          showProgress: true
-        }
-      },
+			},
+
+			preview: {
+				files : { './': 'upload/index.shtml' },
+				options: {
+					path: '/usr/yorkweb/web/preview/',
+					host: 'sftp.york.ac.uk',
+					username: '<%= credentials.preview.username %>',
+					password: '<%= credentials.preview.password %>',
+					srcBasePath: 'upload/',
+					showProgress: true
+				}
+			},
+
+			live: {
+				files : { './': 'upload/index.shtml' },
+				options: {
+					path: '.',
+					host: 'sftp.york.ac.uk',
+					username: '<%= credentials.live.username %>',
+					password: '<%= credentials.live.password %>',
+					srcBasePath: 'upload/',
+					showProgress: true
+				}
+			},
+
+			images: {
+				files : { './': 'upload/images/**' },
+				options: {
+					path: '/usr/yorkweb/web/static/data/homepage/images/',
+					host: 'sftp.york.ac.uk',
+					username: '<%= credentials.static.username %>',
+					password: '<%= credentials.static.password %>',
+					srcBasePath: 'upload/images/',
+					showProgress: true
+				}
+			},
+
 			screenshot: {
-				files : {
-					'./': 'upload/screenshots/**'
-				},
+				files : { './': 'upload/screenshots/**' },
 				options: {
 					path: 'screenshots',
 					host: 'sftp.york.ac.uk',
@@ -100,70 +96,67 @@ module.exports = function(grunt) {
 					src:'https://www.york.ac.uk',
 					dest:'upload/screenshots/homepage_' + dateStamp +'.jpg',
 					delay:1000,
-          viewport: '1024x1000'
+					viewport: '1024x1000'
 				}
 			}
 		},
 
 		open: {
-			test: {
-				path:'https://www.york.ac.uk/static/data/homepage/'
-			},
-			live: {
-				path:'https://www.york.ac.uk/'
-			}
-
+			test: { path:'https://www.york.ac.uk/static/data/homepage/' },
+			live: { path:'https://www.york.ac.uk/' }
 		},
 
 		confirm: {
-    	sftp: {
-      	options: {
-        	// Static text.
-        	question: 'This will deploy your local copy of index.html to the live site.\nHave you tested it? If in doubt, run "grunt test" again. (y/n)',
-        	continue: function(answer) {
-          	return answer.toLowerCase() === 'y';
-        	}
-      	}
-    	}
-  	},
+			sftp: {
+				options: {
+					// Static text.
+					question: 'This will deploy your local copy of index.html to the live site.\nHave you tested it? If in doubt, run "grunt test" again. (y/n)',
+					continue: function(answer) { return answer.toLowerCase() === 'y'; }
+				}
+			}
+		},
 
-    get: {
-      api: {
-        layoutDir: 'layouts',
-        targetDir: 'download',
-        uploadDir: 'upload'
-      }
-    }
+		get: {
+			api: {
+				layoutDir: 'layouts',
+				targetDir: 'download',
+				uploadDir: 'upload'
+			}
+		}
 
 	});
 
 	// Load the plugins
 	require('load-grunt-tasks')(grunt);
-  grunt.loadTasks('tasks');
+	grunt.loadTasks('tasks');
 
-	//Tasks
-  grunt.registerTask('default',[
-    'clean',
-    'get',
-    'bake'
-  ]);
-  grunt.registerTask('test',[
-    'default',
-    'sftp:images',
-    'sftp:test',
-    'open:test'
-  ]);
+	// Tasks
+
+	grunt.registerTask('default',[
+		'clean',
+		'get',
+		'bake'
+	]);
+	
+	grunt.registerTask('test',[
+		'default',
+		'sftp:images',
+		'sftp:test',
+		'open:test'
+	]);
+	
 	grunt.registerTask('live',[
 		'confirm',
-    'sftp:live',
+		'sftp:live',
 		'screenshot',
 		'sftp:screenshot',
 		'open:live'
 	]);
-    grunt.registerTask('livedeploy',[
-        'sftp:live',
-        'screenshot',
-        'sftp:screenshot',
-        'open:live'
-    ]);
+
+	grunt.registerTask('livedeploy',[
+		'sftp:live',
+		'screenshot',
+		'sftp:screenshot',
+		'open:live'
+	]);
 };
